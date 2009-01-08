9c476633854b57f3262e13fe3716208e523a4067
Jan Luehe <jluehe@java.net>
2009-01-08 00:55:21 +0000

    Fixed formatting/indentation and removed tabs. No functional change.
    svn path=/trunk/v3/web/javax.servlet/; revision=24200

--- src/main/java/javax/servlet/RequestDispatcher.java
+++ src/main/java/javax/servlet/RequestDispatcher.java
@@ -51,14 +51,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-
-
-
 package javax.servlet;
 
 import java.io.IOException;
-
 
 /**
  * Defines an object that receives requests from the client
@@ -72,67 +67,55 @@ import java.io.IOException;
  * but a servlet container can create <code>RequestDispatcher</code>
  * objects to wrap any type of resource.
  *
- * @author 	Various
+ * @author Various
  *
- * @see 	ServletContext#getRequestDispatcher(java.lang.String)
- * @see 	ServletContext#getNamedDispatcher(java.lang.String)
- * @see 	ServletRequest#getRequestDispatcher(java.lang.String)
- *
+ * @see ServletContext#getRequestDispatcher(java.lang.String)
+ * @see ServletContext#getNamedDispatcher(java.lang.String)
+ * @see ServletRequest#getRequestDispatcher(java.lang.String)
  */
  
 public interface RequestDispatcher {
 
-
-
-
-
-/**
- * Forwards a request from
- * a servlet to another resource (servlet, JSP file, or
- * HTML file) on the server. This method allows
- * one servlet to do preliminary processing of
- * a request and another resource to generate
- * the response.
- *
- * <p>For a <code>RequestDispatcher</code> obtained via 
- * <code>getRequestDispatcher()</code>, the <code>ServletRequest</code> 
- * object has its path elements and parameters adjusted to match
- * the path of the target resource.
- *
- * <p><code>forward</code> should be called before the response has been 
- * committed to the client (before response body output has been flushed).  
- * If the response already has been committed, this method throws
- * an <code>IllegalStateException</code>.
- * Uncommitted output in the response buffer is automatically cleared 
- * before the forward.
- *
- * <p>The request and response parameters must be either the same
- * objects as were passed to the calling servlet's service method or be
- * subclasses of the {@link ServletRequestWrapper} or {@link ServletResponseWrapper} classes
- * that wrap them.
- *
- *
- * @param request		a {@link ServletRequest} object
- *				that represents the request the client
- * 				makes of the servlet
- *
- * @param response		a {@link ServletResponse} object
- *				that represents the response the servlet
- *				returns to the client
- *
- * @exception ServletException	if the target resource throws this exception
- *
- * @exception IOException	if the target resource throws this exception
- *
- * @exception IllegalStateException	if the response was already committed
- *
- */
-
+    /**
+     * Forwards a request from
+     * a servlet to another resource (servlet, JSP file, or
+     * HTML file) on the server. This method allows
+     * one servlet to do preliminary processing of
+     * a request and another resource to generate
+     * the response.
+     *
+     * <p>For a <code>RequestDispatcher</code> obtained via 
+     * <code>getRequestDispatcher()</code>, the <code>ServletRequest</code> 
+     * object has its path elements and parameters adjusted to match
+     * the path of the target resource.
+     *
+     * <p><code>forward</code> should be called before the response has been 
+     * committed to the client (before response body output has been flushed). 
+     * If the response already has been committed, this method throws
+     * an <code>IllegalStateException</code>.
+     * Uncommitted output in the response buffer is automatically cleared 
+     * before the forward.
+     *
+     * <p>The request and response parameters must be either the same
+     * objects as were passed to the calling servlet's service method or be
+     * subclasses of the {@link ServletRequestWrapper} or
+     * {@link ServletResponseWrapper} classes
+     * that wrap them.
+     *
+     * @param request a {@link ServletRequest} object that represents the
+     * request the client makes of the servlet
+     *
+     * @param response a {@link ServletResponse} object that represents
+     * the response the servlet returns to the client
+     *
+     * @throws ServletException if the target resource throws this exception
+     *
+     * @throws IOException if the target resource throws this exception
+     *
+     * @throws IllegalStateException if the response was already committed
+     */
     public void forward(ServletRequest request, ServletResponse response)
-	throws ServletException, IOException;
-
-
-
+        throws ServletException, IOException;
 
     /**
      *
@@ -147,26 +130,22 @@ public interface RequestDispatcher {
      *
      * <p>The request and response parameters must be either the same
      * objects as were passed to the calling servlet's service method or be
-     * subclasses of the {@link ServletRequestWrapper} or {@link ServletResponseWrapper} classes
-     * that wrap them.
-     * 
+     * subclasses of the {@link ServletRequestWrapper} or
+     * {@link ServletResponseWrapper} classes that wrap them.
      *
+     * @param request a {@link ServletRequest} object that contains the
+     * client's request
      *
-     * @param request 			a {@link ServletRequest} object 
-     *					that contains the client's request
+     * @param response a {@link ServletResponse} object that contains the
+     * servlet's response
      *
-     * @param response 			a {@link ServletResponse} object 
-     * 					that contains the servlet's response
+     * @throws ServletException if the included resource throws this
+     * exception
      *
-     * @exception ServletException 	if the included resource throws this exception
-     *
-     * @exception IOException 		if the included resource throws this exception
-     *
-     *
+     * @throws IOException if the included resource throws this exception
      */
-     
     public void include(ServletRequest request, ServletResponse response)
-	throws ServletException, IOException;
+        throws ServletException, IOException;
 }
 
 
