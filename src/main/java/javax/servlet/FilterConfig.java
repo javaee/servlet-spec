7060ad522c60b1d47bd434ab790a475e09fd58ea
Jan Luehe <jluehe@java.net>
2009-03-05 20:03:41 +0000

    Fixed indentation
    svn path=/trunk/v3/web/javax.servlet/; revision=25131

--- src/main/java/javax/servlet/FilterConfig.java
+++ src/main/java/javax/servlet/FilterConfig.java
@@ -52,61 +52,50 @@
  * limitations under the License.
  */
 
-
-
-
 package javax.servlet;
-
 
 import java.util.Enumeration;
 
-	 /** 
-	 *
-	 * A filter configuration object used by a servlet container
-	 * to pass information to a filter during initialization.
-	 * @see Filter 
-	  * @since	Servlet 2.3
-	 *
-	 */
-
+/** 
+ * A filter configuration object used by a servlet container
+ * to pass information to a filter during initialization.
+ *
+ * @see Filter 
+ * @since Servlet 2.3
+ */
 
 public interface FilterConfig {
 
-	/** 
-	* Returns the filter-name of this filter as defined in the deployment descriptor. 
-	*/
-	
-	public String getFilterName();
+    /** 
+     * Returns the filter-name of this filter as defined in the deployment
+     * descriptor. 
+     */
+    public String getFilterName();
 
 
- /**
+    /**
      * Returns a reference to the {@link ServletContext} in which the caller
      * is executing.
      *
-     *
-     * @return		a {@link ServletContext} object, used
-     *			by the caller to interact with its servlet 
-     *                  container
+     * @return a {@link ServletContext} object, used by the caller to
+     * interact with its servlet container
      * 
-     * @see		ServletContext
-     *
+     * @see ServletContext
      */
-
     public ServletContext getServletContext();
     
+
     /**
      * Returns a <code>String</code> containing the value of the 
      * named initialization parameter, or <code>null</code> if 
      * the parameter does not exist.
      *
-     * @param name	a <code>String</code> specifying the name
-     *			of the initialization parameter
+     * @param name a <code>String</code> specifying the name of the
+     * initialization parameter
      *
-     * @return		a <code>String</code> containing the value 
-     *			of the initialization parameter
-     *
+     * @return a <code>String</code> containing the value of the
+     * initialization parameter
      */
-
     public String getInitParameter(String name);
 
 
@@ -116,17 +105,8 @@ public interface FilterConfig {
      * or an empty <code>Enumeration</code> if the filter has
      * no initialization parameters.
      *
-     * @return		an <code>Enumeration</code> of <code>String</code> 
-     *			objects containing the names of the filter's 
-     *			initialization parameters
-     *
-     *
-     *
+     * @return an <code>Enumeration</code> of <code>String</code> objects
+     * containing the names of the filter's initialization parameters
      */
-
     public Enumeration getInitParameterNames();
-
-
-
-
 }
