ea5a1416b8c886463b2463aca11d4efc61d8a5c7
Rajiv Mordani <mode@java.net>
2009-03-16 18:30:22 +0000

    Fixed license header
    svn path=/trunk/v3/web/javax.servlet/; revision=25427

--- src/main/java/javax/servlet/annotation/WebServlet.java
+++ src/main/java/javax/servlet/annotation/WebServlet.java
@@ -20,9 +20,20 @@
  * identifying information: "Portions Copyrighted [year]
  * [name of copyright owner]"
  *
- * Copyright 2008 Sun Microsystems, Inc. All rights reserved.
+ * Contributor(s):
  *
+ * If you wish your version of this file to be governed by only the CDDL or
+ * only the GPL Version 2, indicate your decision by adding "[Contributor]
+ * elects to include this software in this distribution under the [CDDL or GPL
+ * Version 2] license."  If you don't indicate a single choice of license, a
+ * recipient has the option to distribute your version of this file under
+ * either the CDDL, the GPL Version 2 or to extend the choice of license to
+ * its licensees as provided above.  However, if you add GPL Version 2 code
+ * and therefore, elected the GPL Version 2 license, then the option applies
+ * only if the new code is made subject to such option by the copyright
+ * holder.
  */
+
 
 package javax.servlet.annotation;
 
