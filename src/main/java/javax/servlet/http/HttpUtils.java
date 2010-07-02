7b6220b8ac9b0ff3261de232ad816e6c0124f070
Shing Wai Chan <shing.wai.chan@oracle.com>
2010-07-02 01:36:03 +0000

    Use ContextClassLoader to load PersistenceStrategyBuilder
    svn path=/trunk/v3/web/javax.servlet/; revision=38289

--- src/main/java/javax/servlet/http/HttpUtils.java
+++ src/main/java/javax/servlet/http/HttpUtils.java
@@ -111,37 +111,38 @@ public class HttpUtils {
      */
     public static Hashtable<String, String[]> parseQueryString(String s) {
 
-	String valArray[] = null;
+        String valArray[] = null;
 	
-	if (s == null) {
-	    throw new IllegalArgumentException();
-	}
+        if (s == null) {
+            throw new IllegalArgumentException();
+        }
 
-	Hashtable<String, String[]> ht = new Hashtable<String, String[]>();
-	StringBuffer sb = new StringBuffer();
-	StringTokenizer st = new StringTokenizer(s, "&");
-	while (st.hasMoreTokens()) {
-	    String pair = st.nextToken();
-	    int pos = pair.indexOf('=');
-	    if (pos == -1) {
-		// XXX
-		// should give more detail about the illegal argument
-		throw new IllegalArgumentException();
-	    }
-	    String key = parseName(pair.substring(0, pos), sb);
-	    String val = parseName(pair.substring(pos+1, pair.length()), sb);
-	    if (ht.containsKey(key)) {
-		String oldVals[] = ht.get(key);
-		valArray = new String[oldVals.length + 1];
-		for (int i = 0; i < oldVals.length; i++) 
-		    valArray[i] = oldVals[i];
-		valArray[oldVals.length] = val;
-	    } else {
-		valArray = new String[1];
-		valArray[0] = val;
-	    }
-	    ht.put(key, valArray);
-	}
+        Hashtable<String, String[]> ht = new Hashtable<String, String[]>();
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(s, "&");
+        while (st.hasMoreTokens()) {
+        String pair = st.nextToken();
+        int pos = pair.indexOf('=');
+        if (pos == -1) {
+            // XXX
+            // should give more detail about the illegal argument
+            throw new IllegalArgumentException();
+        }
+        String key = parseName(pair.substring(0, pos), sb);
+        String val = parseName(pair.substring(pos+1, pair.length()), sb);
+        if (ht.containsKey(key)) {
+            String oldVals[] = ht.get(key);
+            valArray = new String[oldVals.length + 1];
+            for (int i = 0; i < oldVals.length; i++) {
+                valArray[i] = oldVals[i];
+            }
+            valArray[oldVals.length] = val;
+        } else {
+            valArray = new String[1];
+            valArray[0] = val;
+        }
+        ht.put(key, valArray);
+    }
 
 	return ht;
     }
@@ -235,38 +236,38 @@ public class HttpUtils {
     /*
      * Parse a name in the query string.
      */
-    private static String parseName(String s, StringBuffer sb) {
-	sb.setLength(0);
-	for (int i = 0; i < s.length(); i++) {
-	    char c = s.charAt(i); 
-	    switch (c) {
-	    case '+':
-		sb.append(' ');
-		break;
-	    case '%':
-		try {
-		    sb.append((char) Integer.parseInt(s.substring(i+1, i+3), 
-						      16));
-		    i += 2;
-		} catch (NumberFormatException e) {
-		    // XXX
-		    // need to be more specific about illegal arg
-		    throw new IllegalArgumentException();
-		} catch (StringIndexOutOfBoundsException e) {
-		    String rest  = s.substring(i);
-		    sb.append(rest);
-		    if (rest.length()==2)
-			i++;
-		}
-		
-		break;
-	    default:
-		sb.append(c);
-		break;
-	    }
-	}
+    private static String parseName(String s, StringBuilder sb) {
+        sb.setLength(0);
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i); 
+            switch (c) {
+                case '+':
+                    sb.append(' ');
+                    break;
+                case '%':
+                    try {
+                        sb.append((char) Integer.parseInt(s.substring(i+1, i+3), 
+                                16));
+                        i += 2;
+                    } catch (NumberFormatException e) {
+                        // XXX
+                        // need to be more specific about illegal arg
+                        throw new IllegalArgumentException();
+                    } catch (StringIndexOutOfBoundsException e) {
+                        String rest  = s.substring(i);
+                        sb.append(rest);
+                        if (rest.length()==2)
+                            i++;
+                        }
 
-	return sb.toString();
+                        break;
+                default:
+                    sb.append(c);
+                    break;
+            }
+        }
+
+        return sb.toString();
     }
 
 
@@ -292,29 +293,29 @@ public class HttpUtils {
      *			the reconstructed URL
      */
     public static StringBuffer getRequestURL (HttpServletRequest req) {
-	StringBuffer url = new StringBuffer ();
-	String scheme = req.getScheme ();
-	int port = req.getServerPort ();
-	String urlPath = req.getRequestURI();
-	
-	//String		servletPath = req.getServletPath ();
-	//String		pathInfo = req.getPathInfo ();
+        StringBuffer url = new StringBuffer();
+        String scheme = req.getScheme ();
+        int port = req.getServerPort ();
+        String urlPath = req.getRequestURI();
 
-	url.append (scheme);		// http, https
-	url.append ("://");
-	url.append (req.getServerName ());
-	if ((scheme.equals ("http") && port != 80)
-		|| (scheme.equals ("https") && port != 443)) {
-	    url.append (':');
-	    url.append (req.getServerPort ());
-	}
-	//if (servletPath != null)
-	//    url.append (servletPath);
-	//if (pathInfo != null)
-	//    url.append (pathInfo);
-	url.append(urlPath);
+        //String		servletPath = req.getServletPath ();
+        //String		pathInfo = req.getPathInfo ();
 
-	return url;
+        url.append (scheme);		// http, https
+        url.append ("://");
+        url.append (req.getServerName ());
+        if ((scheme.equals ("http") && port != 80)
+        || (scheme.equals ("https") && port != 443)) {
+            url.append (':');
+            url.append (req.getServerPort ());
+        }
+        //if (servletPath != null)
+        //    url.append (servletPath);
+        //if (pathInfo != null)
+        //    url.append (pathInfo);
+        url.append(urlPath);
+
+        return url;
     }
 }
 
