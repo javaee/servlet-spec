d50624f4931fdfc76da27b08a4d2bb412d3cc685
Shing Wai Chan <shing.wai.chan@oracle.com>
2011-07-06 23:47:22 +0000

    fix spaces
    svn path=/trunk/v3/web/javax.servlet/; revision=47894

--- src/main/java/javax/servlet/ServletRequestEvent.java
+++ src/main/java/javax/servlet/ServletRequestEvent.java
@@ -55,33 +55,29 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package javax.servlet;
 
-
-    /** 
-      * Events of this kind indicate lifecycle
-      * events for a ServletRequest.
-      * The source of the event
-      * is the ServletContext of this web application.
-      * @see ServletRequestListener
-      * @since Servlet 2.4
-      */
-
+/** 
+ * Events of this kind indicate lifecycle events for a ServletRequest.
+ * The source of the event is the ServletContext of this web application.
+ *
+ * @see ServletRequestListener
+ * @since Servlet 2.4
+ */
 public class ServletRequestEvent extends java.util.EventObject { 
     private final transient ServletRequest request;
 
     /** Construct a ServletRequestEvent for the given ServletContext
       * and ServletRequest.
       *
-      * @param sc		the ServletContext of the web application.
-      * @param request		the ServletRequest that is sending the event.
+      * @param sc       the ServletContext of the web application.
+      * @param request  the ServletRequest that is sending the event.
       */
     public ServletRequestEvent(ServletContext sc, ServletRequest request) {
         super(sc);
         this.request = request;
     }
-    
+
     /**
       * Returns the ServletRequest that is changing.
       */
