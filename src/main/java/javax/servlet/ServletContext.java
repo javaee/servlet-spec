08a75285ede4c7da25b42f29efef547e7cd11f4c
Shing Wai Chan <shing.wai.chan@oracle.com>
2013-02-06 23:59:55 +0000

    fix javadoc in getContextPath according to section 3.5 in spec
    svn path=/trunk/api/javaee-api/javax.servlet/; revision=59212

--- src/main/java/javax/servlet/ServletContext.java
+++ src/main/java/javax/servlet/ServletContext.java
@@ -123,9 +123,11 @@ public interface ServletContext {
      *
      * <p>The context path is the portion of the request URI that is used
      * to select the context of the request. The context path always comes
-     * first in a request URI. The path starts with a <tt>/</tt> character
-     * but does not end with a <tt>/</tt> character. For servlets in the
-     * default (root) context, this method returns "".
+     * first in a request URI. If this context is the “default” context
+     * rooted at the base of the Web server’s URL name space, this path
+     * will be an empty string. Otherwise, if the context is not rooted at
+     * the root of the server’s name space, the path starts with a /
+     * character but does not end with a / character.
      *
      * <p>It is possible that a servlet container may match a context by
      * more than one context path. In such cases the
