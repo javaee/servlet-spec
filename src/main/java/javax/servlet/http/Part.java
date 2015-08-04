60b0425c4b58b1fae0f4a2e8dad32f0ec9b15ddc
Shing Wai Chan <shing.wai.chan@oracle.com>
2015-08-04 22:40:11 +0000

    fix typo in javadoc
    svn path=/trunk/api/javaee-api/javax.servlet/; revision=64020

--- src/main/java/javax/servlet/http/Part.java
+++ src/main/java/javax/servlet/http/Part.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 1997-2013 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997-2015 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -55,7 +55,7 @@ public interface Part {
      * Gets the content of this part as an <tt>InputStream</tt>
      * 
      * @return The content of this part as an <tt>InputStream</tt>
-     * @throws IOException If an error occurs in retrieving the contet
+     * @throws IOException If an error occurs in retrieving the content
      * as an <tt>InputStream</tt>
      */
     public InputStream getInputStream() throws IOException;
